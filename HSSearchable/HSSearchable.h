44f267d1ca6d4b3a57b93e6e3dfc25611dbff612
Hitendra Solanki <hitendra.spaceo@gmail.com>
2017-08-29 12:45:38 +0530

    Initial Commit

---
+++ HSSearchable/HSSearchable.h
@@ -0,0 +1,19 @@
+//
+//  HSSearchable.h
+//  HSSearchable
+//
+//  Created by SOTSYS216 on 29/08/17.
+//  Copyright © 2017 Hitendra iDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HSSearchable.
+FOUNDATION_EXPORT double HSSearchableVersionNumber;
+
+//! Project version string for HSSearchable.
+FOUNDATION_EXPORT const unsigned char HSSearchableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSSearchable/PublicHeader.h>
+
+
